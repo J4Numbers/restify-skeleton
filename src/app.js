1c94ed1bce64ef5e6c823124e07d09f0a95fa181
Jayne <j4numbers@gmail.com>
2019-09-18 18:02:32 +0100

    refactor(app): bring in a more concrete structure for the skeleton

--- src/app.js
+++ src/app.js
@@ -1,5 +1,4 @@
 const uuid = require('uuid/v4');
-const config = require('config');
 const restify = require('restify');
 
 const loggerEngine = require('./lib/logger');
@@ -7,33 +6,43 @@ const routingEngine = require('./lib/routes');
 const onEventHandlers = require('./lib/middleware/on_handlers');
 const preRequestHandlers = require('./lib/middleware/pre_handlers');
 
-const log = loggerEngine.bunyanLogger();
 
-const server = restify.createServer({
-  name: config.get('app.name'),
-  url: config.get('app.hostname'),
-  ignoreTrailingSlash: true,
-  log,
-});
+const load = async (props) => {
+  const log = loggerEngine.bunyanLogger();
+  const server = restify.createServer({
+    name:                props.app.name,
+    url:                 props.app.hostname,
+    ignoreTrailingSlash: true,
+    log,
+  });
 
-server.pre(restify.plugins.pre.dedupeSlashes());
-server.pre(restify.plugins.pre.sanitizePath());
-preRequestHandlers(server);
+  server.pre(restify.plugins.pre.dedupeSlashes());
+  server.pre(restify.plugins.pre.sanitizePath());
+  preRequestHandlers(server);
 
-server.use(restify.plugins.acceptParser(server.acceptable));
-server.use(restify.plugins.queryParser());
-server.use(restify.plugins.bodyParser());
+  server.use(restify.plugins.acceptParser(server.acceptable));
+  server.use(restify.plugins.queryParser());
+  server.use(restify.plugins.bodyParser());
 
-server.use(restify.plugins.requestLogger({
-  properties: {
-    'correlation-id': uuid(),
-  },
-}));
+  server.use(restify.plugins.requestLogger({
+    properties: {
+      'correlation-id': uuid(),
+    },
+  }));
 
-onEventHandlers(server);
+  onEventHandlers(server);
+  routingEngine(server);
 
-routingEngine(server);
+  return server;
+};
 
-server.listen(config.get('app.port'), config.get('app.hostname'), () => {
-  log.info(`${server.name} listening at ${server.url}`);
-});
+const start = (server, props) => {
+  server.listen(props.app.port, props.app.hostname, () => {
+    server.log.info(`${server.name} listening at ${server.url}`);
+  });
+};
+
+module.exports = {
+  load,
+  start,
+};
