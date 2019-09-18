2e80935bd6331991604d6ba44ec8116c8ee79513
Jayne <j4numbers@gmail.com>
2019-09-18 18:18:28 +0100

    refactor(handlers): made the handlers a bit more pass-through-able

--- src/lib/middleware/on_handlers/restify_handler.js
+++ src/lib/middleware/on_handlers/restify_handler.js
@@ -1,5 +1,9 @@
 const restifyHandler = (req, res, err, callback) => {
   req.log.info(`Error thrown within restify: ${err}`);
+  err.toJSON = () => ({
+    message:    err.message,
+    statusCode: res.statusCode,
+  });
   return callback();
 };
 
