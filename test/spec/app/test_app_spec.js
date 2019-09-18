ea1496238e08704e6f3507c6c00be84b44be8d72
Jayne <j4numbers@gmail.com>
2019-09-18 19:22:01 +0100

    feat(https): brought in a test for https resolution that doesn't work
    Added a test which doesn't work with the current version of SuperAgent

--- test/spec/app/test_app_spec.js
+++ test/spec/app/test_app_spec.js
@@ -1,13 +1,30 @@
+const fs = require('fs');
 const testConfig = require('../../../config/test');
+const httpsConfig = require('../../../config/test-https');
 
 describe('The application base layer', function () {
-  describe('The index page', function () {
+  describe('A generic page in http', function () {
     it('should return a 404 not found', async function () {
       const app = testRequire('app');
       const server = await app.load(testConfig);
       app.start(server, testConfig);
       return request(server)
-        .get('/')
+        .get('/this-page-will-never-exist')
+        .then((response) => {
+          expect(response).to.have.status(404);
+        });
+    });
+  });
+
+  describe.skip('A generic page in https', function () {
+    it('should return a 404 not found', async function () {
+      const app = testRequire('app');
+      const server = await app.load(httpsConfig);
+      app.start(server, httpsConfig);
+      return request(server)
+        .key(fs.readFileSync(httpsConfig.app.http2.key))
+        .cert(fs.readFileSync(httpsConfig.app.http2.cert))
+        .get('/this-page-will-never-exist')
         .then((response) => {
           expect(response).to.have.status(404);
         });
